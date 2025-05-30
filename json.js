fd5bb862befd8896dcf7b6befb44bb2b0884d0d8
peter-frank-hds <156174829+peter-frank-hds@users.noreply.github.com>
2025-05-30 15:02:21 +0200

    Update json.js

--- json.js
+++ json.js
@@ -23,46 +23,6 @@ const json = {
           "type": "text",
           "name": "Frage 1",
           "title": "Was ist Ihnen bei der Zusammenarbeit wichtig?"
-        },
-        {
-          "type": "text",
-          "name": "question1",
-          "title": "Welche Herausforderungen möchten Sie lösen?"
-        },
-        {
-          "type": "text",
-          "name": "question3",
-          "title": "Welche Probleme stellen sich dar? "
-        },
-        {
-          "type": "tagbox",
-          "name": "question4",
-          "title": "Welches unserer Angebote interessiert Sie am meisten?",
-          "choices": [
-            {
-              "value": "Item 2",
-              "text": "Option B: Produkt B Item 2"
-            },
-            {
-              "value": "Item 3",
-              "text": "Option C: Andere Item 3"
-            },
-            {
-              "value": "Item 4",
-              "text": "Option A: Produkt A"
-            }
-          ],
-          "showNoneItem": true,
-          "showSelectAllItem": true
-        },
-        {
-          "type": "ranking",
-          "name": "question5",
-          "choices": [
-            "Item 1",
-            "Item 2",
-            "Item 3"
-          ]
         }
       ]
     }
