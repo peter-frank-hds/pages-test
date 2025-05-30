ab9bc83594e4bad3e13d8f3ae6a774914964bf70
peter-frank-hds <156174829+peter-frank-hds@users.noreply.github.com>
2025-05-30 14:19:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+function getQueryParam(name) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name);
+}
+
+const entryId = getQueryParam("id");
+
+// Always create the model
 const survey = new Survey.Model(json);
+
+// Try to load data if ID is present
+if (entryId) {
+  const savedData = localStorage.getItem("survey_" + entryId);
+  if (savedData) {
+    survey.data = JSON.parse(savedData);
+  }
+}
+
+// Apply theme and render
 survey.applyTheme(themeJson);
 survey.onComplete.add((sender, options) => {
-    console.log(JSON.stringify(sender.data, null, 3));
+  console.log("Survey result:", JSON.stringify(sender.data, null, 2));
 });
-$("#surveyElement").Survey({ model: survey });
+
+// Render the survey
+$(document).ready(function () {
+  $("#surveyElement").Survey({ model: survey });
+});
+
+// Save on value change
+survey.onValueChanged.add(function () {
+  if (entryId) {
+    localStorage.setItem("survey_" + entryId, JSON.stringify(survey.data));
+  }
+});
