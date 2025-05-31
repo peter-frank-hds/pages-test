fcb613bc17ee7abd43c18c7bb38d96c3bf134e60
peter-frank-hds <156174829+peter-frank-hds@users.noreply.github.com>
2025-05-31 08:28:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -46,13 +46,37 @@ function renderSurvey() {
     // Create a new button
     const saveBtn = document.createElement("input");
     saveBtn.type = "button";
-    saveBtn.id = "saveButton";
+    saveBtn.id = "saveButton2";
     saveBtn.className = completeButton.className; // same style
     saveBtn.value = "Speichern";
     saveBtn.title = "Speichern";
     saveBtn.style.marginLeft = "20px";
     // Insert the button after the complete button
     completeButton.parentNode.insertBefore(saveBtn, completeButton.nextSibling);
+    
+    document.getElementById("saveButton2").addEventListener("click", function () {
+    const dataToSave = survey.data;
+    if (entryId) {
+        localStorage.setItem("survey_" + entryId, JSON.stringify(dataToSave));
+
+        console.log("Formular wurde gespeichert.");
+        fetch("https://prod-83.westeurope.logic.azure.com:443/workflows/27e380ff4382426d8128f59e5f032ea4/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=RcWZ2ujw6J0hGYCnVZWT9txXu_b2oVKqVdvcwFcSyXU", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                submissionId: entryId, // your existing ID or null
+                operation: "save",
+                payload: survey.data,
+                status: "Draft"
+            })
+        });
+
+    } else {
+        console.log("Kein Eintrag-ID gefunden. Speichern nicht möglich.");
+    }
+}
   }
 }
 
